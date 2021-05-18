1c9aeb0b22d17c0b9527b42d83d7497d7ade7c9c
Irtiaz <irtiazkabir.irtiaz@gmail.com>
2021-05-18 14:53:05 +0600

    Navbar added

---
+++ Components/Navbar.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import Link from 'next/Link';
+
+import Container from './Container.js';
+
+import styles from '../styles/Navbar.module.css';
+
+export default class Navbar extends React.Component {
+	render() {
+		return (
+			<header className={styles.header}>
+				<Container>
+					<div className={styles.navContainer}>
+
+						<div>
+							<h1>
+								<Link href="/">
+									<a>LOGO</a>
+								</Link>
+							</h1>
+							<div>We are the best</div>
+						</div>
+
+						<nav>
+							<ul>
+								<li>
+									<Link href="#">
+										<a>Sign Up</a>
+									</Link>
+								</li>
+								<li>
+									<Link href="#">
+										<a>Sign In</a>
+									</Link>
+								</li>
+								<li>
+									<Link href="#">
+										<a>Contact Us</a>
+									</Link>
+								</li>
+							</ul>
+						</nav>
+
+
+					</div>
+				</Container>
+			</header>
+		);
+	}
+}
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,19 @@
-import '../styles/globals.css'
+import Head from 'next/head';
+
+import Navbar from '../Components/Navbar.js';
+
+import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+	return (
+		<>
+			<Head>
+				<title>E-mart</title>
+			</Head>
+			<Navbar />
+			<Component {...pageProps} />
+		</>
+	);
 }
 
 export default MyApp
